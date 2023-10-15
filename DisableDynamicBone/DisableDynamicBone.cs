6347f3c6e22b8de754d54a69f3f3b0c01d9c1a8d
rassi0429 <taitokokoa+rassi@gmail.com>
2023-10-15 12:49:00 +0900

    feat: kansei

---
+++ DisableDynamicBone/DisableDynamicBone.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisableDynamicBone
+{
+    public class DisableDynamicBone: ResoniteMod
+    {
+        public override string Name => "DisableDynamicBone";
+        public override string Author => "kokoa";
+        public override string Version => "1.0.0";
+        public override string Link => "";
+
+        internal static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> disableDynamicBone = new ModConfigurationKey<bool>("disable", "disable dynamic bone", () => false);
+
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            Harmony harmony = new Harmony("com.kokoa.DisableDynamicBone");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FrooxEngine.DynamicBoneChainManager), "Update", new Type[] { })]
+        class Patch
+        {
+            static bool Prefix()
+            {
+                if (config.GetValue(disableDynamicBone))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
